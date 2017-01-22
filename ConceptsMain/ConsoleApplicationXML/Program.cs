d59d80313a1c1558ff2ddcaa6f4e56002dc64ed3
Vaibhav <Vaibhav@VIBS-STUDIO>
2017-01-22 21:58:29 +0530

    Added a simple string which contains a XML. Read the XML using XDocument class static method XDocument.parse(stringname). After that went into a loop to display these values.

--- ConceptsMain/ConsoleApplicationXML/Program.cs
+++ ConceptsMain/ConsoleApplicationXML/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
+
 
 namespace ConsoleApplicationXML
 {
@@ -9,6 +11,27 @@ namespace ConsoleApplicationXML
     {
         static void Main(string[] args)
         {
+
+            string myXML = @"<Departments>
+                       <Department>Account</Department>
+                       <Department>Sales</Department>
+                       <Department>Pre-Sales</Department>
+                       <Department>Marketing</Department>
+                       </Departments>";
+
+            XDocument xdoc = new XDocument();
+            xdoc = XDocument.Parse(myXML);
+
+            var allDecendantsOfDepartments = xdoc.Element("Departments").Descendants();
+
+            foreach (XElement dept in allDecendantsOfDepartments)
+            {
+                Console.WriteLine("Department Name is {0}", dept.Value);
+
+            }
+
+
+
             Console.ReadLine();
 
         }
