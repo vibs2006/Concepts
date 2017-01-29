79f787d7cd625094a4d1c5fb137b538a8ec2f6f4
Vaibhav <Vaibhav@VIBS-STUDIO>
2017-01-29 12:17:43 +0530

    Added a Sample MVC5 Web Application.

---
+++ ConceptsMain/WebApplication20170129.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication20170129.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ConceptsMain/WebApplication20170129/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication20170129.Global" Language="C#" %>
