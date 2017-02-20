e0c1c37d43236a6d297c8d2e8e3befe099e7f73b
Vaibhav <Vaibhav@VIBS-STUDIO>
2017-02-20 19:41:01 +0530

    Lab 6 attained.

--- ConceptsMain/Concepts/AngularJS/Questpond/LAB1/customJS/Controllers.js
+++ ConceptsMain/Concepts/AngularJS/Questpond/LAB1/customJS/Controllers.js
@@ -2,16 +2,35 @@
 /// <reference path="../Scripts/angular-mocks.js" />
 
 
-var BindingCode = function ($scope,Factory,UtilityObject)
+var BindingCode = function ($scope,Factory,UtilityObject,$http,$q)
 {
     //$scope.cname = "Vaibhav";
     //$scope.cid = "1001";
     //$scope.CustomerBackgroundColor = "Blue";
     //$scope.Amount = "100";
 
+    $scope.successFn = function (response) {
+        $scope.lstCust = response.data;
+    };
+
+    $scope.failureFn = function (response) {
+        // $scope.lstCust = response.data;
+        alert("Failure - See Console");
+        consol.log(response);
+    };
+
+    $scope.notificationFn = function () {
+        alert("This is the nofication");
+    };
+
     $scope.cust = Factory.CreateCustObject(1); //Dependency Injection
     $scope.util = UtilityObject;
-    
+    $scope.lstCust = [];
+
+    var defer = $q.defer();
+    var promise = defer.promise;
+
+    promise.then($scope.successFn, $scope.failureFn, $scope.notificationFn);
    
 
     $scope.$watch("cust.Amount", function () {
@@ -27,11 +46,30 @@ var BindingCode = function ($scope,Factory,UtilityObject)
     });
 
     $scope.Submit = function () {
+
         if ($scope.cust.cname.length == 0) {
             alert("Name is Blank");
         }
-        else {
-            alert("Good Data");
+        else
+        {
+            ///     alert("Good Data");
+            //$http.post("http://localhost:50253/api/Customer", $scope.cust)
+            //    .then(function (data) {
+            //        //alert("success");
+            //        $scope.cust = data.data;
+            //        console.log(data);
+            //        //$scope.cust.cname = "Name Changed";
+            //    });
+
+            $http.get("http://localhost:50253/api/Customer")
+                .then(function (data) {
+                    //alert("success");
+                    //$scope.lstCust = data.data;
+                    //console.log(data);
+                    //$scope.cust.cname = "Name Changed";
+
+                    defer.resolve(data);
+                });
         }
     };
 
---
+++ ConceptsMain/Concepts/AngularJS/Questpond/Lab4/CustomerServer/CustomerServer/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerServer.Models
+{
+    public class Customer
+    {
+        public string cid { get; set; }
+        public string cname { get; set; }
+        public double Amount { get; set; }
+        //public string BackgroundColor { get; set; }
+
+    }
+}
