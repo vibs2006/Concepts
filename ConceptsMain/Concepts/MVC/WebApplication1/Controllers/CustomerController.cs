31b00bd55c9f7ffa1b212d3e61daf2ec7bdac6b7
Vaibhav <Vaibhav@VIBS-STUDIO>
2017-03-02 20:08:39 +0530

    mvc

---
+++ ConceptsMain/Concepts/MVC/WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return RedirectToAction("RegisterCustomer");
+            //return View();
+        }
+
+        [HttpGet]
+        public ActionResult RegisterCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterCustomer(Customer objCust)
+        {
+            if (ModelState.IsValid)
+            {
+                CustomerDBContext _dbcontext = new CustomerDBContext();
+                _dbcontext.Customers.Add(objCust);
+                _dbcontext.SaveChanges();
+                return View("CustomerSaveConfirmation",objCust);
+            }
+            else
+            {
+                return View("RegisterCustomer", objCust);
+            }
+
+
+        }
+
+
+    }
+}
