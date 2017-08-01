814d93a300a93dbd341c8b0fe18ea6dcc444ebc4
Vaibhav Agarwal <v.agarwal@ennovate-software.com>
2017-08-01 17:36:50 +0530

    Added Asp.net Identity Demo from eBook Jon Galloway

---
+++ ConceptsMain/Concepts/MVC/ASPNETIdentityDemo/WebApplication2/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using AspnetIdentityDemo.Models;
+
+namespace AspnetIdentityDemo.Infrastructure
+{
+    public class AppIdentityDbContext: IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDb") //Name of the Connection String
+        {
+
+        }
+
+        static AppIdentityDbContext()
+        {
+            System.Data.Entity.Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSetup(AppIdentityDbContext context)
+        {
+            // initial configuration will go here. 
+        }
+    }
+}
---
+++ ConceptsMain/Concepts/MVC/ASPNETIdentityDemo/WebApplication2/Infrastructure/AppUserManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using AspnetIdentityDemo.Models;
+
+namespace AspnetIdentityDemo.Infrastructure
+{
+    public class AppUserManager : UserManager<AppUser>
+    {
+        public AppUserManager(IUserStore<AppUser> store) : base(store)
+        {
+
+        }
+
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options,IOwinContext context)
+        {
+            AppIdentityDbContext db = context.Get<AppIdentityDbContext>();
+            AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+            return manager;
+        }
+
+    }
+}
