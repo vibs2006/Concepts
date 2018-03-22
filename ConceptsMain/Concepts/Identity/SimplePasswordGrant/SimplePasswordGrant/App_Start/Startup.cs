9cfee67d474ff65e2067ed3181927945c09a767f
Vibs <vibs2006@gmail.com>
2018-03-22 21:48:57 +0530

    Added ASP.NET Identity OAUTH Basic UI

---
+++ ConceptsMain/Concepts/Identity/SimplePasswordGrant/SimplePasswordGrant/App_Start/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using SimplePasswordGrant;
+[assembly: OwinStartup(typeof(Startup))]
+namespace SimplePasswordGrant
+{
+    public class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+        public void Configuration(IAppBuilder app)
+        {
+            OAuthOptions = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new PathString("/token"),
+                Provider = new OAuthAuthorizationServerProvider()
+                {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+                    OnValidateClientAuthentication = async (context) =>
+                    {
+                        context.Validated();
+                    },
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+                    OnGrantResourceOwnerCredentials = async (context) =>
+                    {
+                        if (context.UserName == "test@test.com" && context.Password == "test")
+                        {
+                            ClaimsIdentity oAuthIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+                            context.Validated(oAuthIdentity);
+                        }
+                    }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+                },
+                AllowInsecureHttp = true,
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1)
+            };
+
+            app.UseOAuthBearerTokens(OAuthOptions);
+        }
+    }
+}
---
+++ ConceptsMain/Concepts/Identity/SimplePasswordGrant/SimplePasswordGrant/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimplePasswordGrant.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return Redirect("/index.html");
+        }
+
+        [AllowAnonymous]
+        public ActionResult General()
+        {
+            return Json(new
+            {
+                AccessType = "Anonymous Access"
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AuthorizeAccess()
+        {
+            return Json(new
+            {
+                AccessType = "Authenticated Access"
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
